2a5787971ffda711c3c1a6b6c0b124739fd3ca0a
gustavo2301 <60801126+gustavo2301@users.noreply.github.com>
2020-02-17 21:30:13 -0300

    Desafio 1

---
+++ Desafio1.js
@@ -0,0 +1,20 @@
+var myvar
+myvar=10
+var soma=15+8
+soma = soma +1
+soma = soma*3
+ //Valor da variavel soma é igual a 72
+ var souNinja = true
+ var comida = ["arroz", "feijão", "ovo"]
+ comida[1]
+
+ if (soma === myvar){
+     return true
+ }else{
+     return false
+ }
+
+function theDivisão (a, b){
+    return a/b
+}
+theDivisão(10,2)
